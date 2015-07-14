36c41ecbae5b8c478fc2c16a1609f8b2d9622db8
felipedbr <reinork1@hotmail.com>
2015-07-13 22:35:20 -0300

    League Kings base
    Base PHP da rede social League Kings

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>League Kings</title>
+
+        <!-- Google Web Fonts -->
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+
+        <!-- Bootstrap -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/titulos.css">
+        <link rel="stylesheet" href="css/league.css">
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+<![endif]-->
+    </head>
+    <body>
+        <header class="cd-header">
+            <div id="cd-logo"><a href="#0"><img src="assets/cd-logo.svg" alt="Logo"></a></div>
+
+            <nav class="cd-primary-nav">
+                <ul>
+                    <li><a href="#0">Home</a></li>
+                    <li><a href="#0">Portfolio</a></li>
+                    <li><a href="#0">Contact</a></li>
+                </ul>
+            </nav> <!-- .cd-primary-nav -->
+        </header>
+
+        <section class="cd-hero">
+            <ul class="cd-hero-slider">
+                <li class="selected">
+                    <div class="cd-full-width">
+                        <section class="cd-intro">
+                            <h1 class="cd-headline slide" style="color: #FFF;">
+                                <span>Encontre tudo sobre League of legends: </span>
+                                <span class="cd-words-wrapper">
+                                    <b class="is-visible">Builds</b>
+                                    <b>Taticas</b>
+                                    <b>Dicas</b>
+                                    <b>Recursos</b>
+                                </span>
+                            </h1>
+                        </section> 
+                        <p>Cadastre-se agora e faça parte da maior rede social de League of Legends.</p>
+                        <a href="#" class="btn btn-danger btn-lg">Cadastre-se</a>
+                    </div> <!-- .cd-full-width -->
+                </li>
+
+                <li>
+                    <div class="cd-half-width">
+                        <h2>Slide title here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In consequatur cumque natus!</p>
+                        <a href="#0" class="cd-btn">Start</a>
+                        <a href="#0" class="cd-btn secondary">Learn More</a>
+                    </div> <!-- .cd-half-width -->
+
+                    <div class="cd-half-width cd-img-container">
+                        <img src="assets/tech-1.jpg" alt="tech 1">
+                    </div> <!-- .cd-half-width.cd-img-container -->
+                </li>
+
+                <li>
+                    <div class="cd-half-width cd-img-container">
+                        <img src="assets/tech-2.jpg" alt="tech 2">
+                    </div> <!-- .cd-half-width.cd-img-container -->
+
+                    <div class="cd-half-width">
+                        <h2>Slide title here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In consequatur cumque natus!</p>
+                        <a href="#0" class="cd-btn">Start</a>
+                        <a href="#0" class="cd-btn secondary">Learn More</a>
+                    </div> <!-- .cd-half-width -->
+
+                </li>
+
+                <li class="cd-bg-video">
+                    <div class="cd-full-width">
+                        <h2>Slide title here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, explicabo.</p>
+                        <a href="#0" class="cd-btn">Learn more</a>
+                    </div> <!-- .cd-full-width -->
+
+                    <div class="cd-bg-video-wrapper" data-video="assets/video/video">
+                        <!-- video element will be loaded using jQuery -->
+                    </div> <!-- .cd-bg-video-wrapper -->
+                </li>
+
+                <li>
+                    <div class="cd-full-width">
+                        <h2>Slide title here</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, explicabo.</p>
+                        <a href="#0" class="cd-btn">Start</a>
+                        <a href="#0" class="cd-btn secondary">Learn More</a>
+                    </div> <!-- .cd-full-width -->
+                </li>
+            </ul> <!-- .cd-hero-slider -->
+
+            <div class="cd-slider-nav">
+                <nav>
+                    <span class="cd-marker item-1"></span>
+
+                    <ul>
+                        <li class="selected"><a href="#0">Intro</a></li>
+                        <li><a href="#0">Tech 1</a></li>
+                        <li><a href="#0">Tech 2</a></li>
+                        <li><a href="#0">Video</a></li>
+                        <li><a href="#0">Image</a></li>
+                    </ul>
+                </nav> 
+            </div> <!-- .cd-slider-nav -->
+        </section> <!-- .cd-hero -->
+
+        <main class="cd-main-content">
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias incidunt nemo veniam alias nam nisi distinctio optio error architecto odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+            </p>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias incidunt nemo veniam alias nam nisi distinctio optio error architecto odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit veniam quisquam, rem illum dicta cumque, voluptate fugiat impedit beatae rerum ratione, voluptates nisi magni delectus ab, eaque atque iste. Molestias incidunt nemo veniam alias nam nisi distinctio optio error architecto odit! Illo dicta nulla fugiat distinctio laudantium, corrupti eum unde.
+            </p>
+        </main> <!-- .cd-main-content -->
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- Include all compiled plugins (below), or include individual files as needed -->
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
